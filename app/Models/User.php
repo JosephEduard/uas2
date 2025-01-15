ec551f801e02a85aec51ae4737f6dc3a345c9e0d
JosephEduard <josepheduardulyloni_2327250080@mhs.mdp.ac.id>
2025-01-15 17:21:46 +0700

    Updatte Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
@@ -47,8 +49,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function orders()
+    public function canAccessPanel(Panel $panel): bool
     {
-        return $this->hasMany(Order::class);
+        return str_ends_with($this->email, '@admin.com');
     }
 }
